dd78cc7ffb941416091f41a85c6f72c3438cc436
hornie <hornminer@gmail.com>
2018-08-31 08:04:09 +0300

    Seed node fuckup fix

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -141,8 +141,7 @@ const uint64_t P2P_DEFAULT_INVOKE_TIMEOUT                    = 60 * 2 * 1000; //
 const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          // 5 seconds
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "";
 
-//const char* const SEED_NODES[] = { "149.154.69.143:18522", "149.154.69.139:18522"};
-const char* const SEED_NODES[] = { "127.0.0.1:18522", "127.0.0.1:18722"};
+const char* const SEED_NODES[] = { "149.154.69.143:18522", "149.154.69.139:18522"};
 //testnet params(will be populated later)
 //const char TEST_PUBLIC_ADDRESS_BASE58_PREFIX[]				 ="ya tvou mamku ebal 228"; 
 
