6689d0720372421143c5ca53a054d3b13cdc9a77
hornie <hornminer@gmail.com>
2019-02-26 19:56:10 +0300

    i dont remember what i did lul

--- src/CryptoNoteCore/CachedBlock.cpp
+++ src/CryptoNoteCore/CachedBlock.cpp
@@ -1,4 +1,6 @@
 // Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+// Copyright (c) 2018, The Monero developers, The Turtlecoin developers
+// Copyright (c) 2018, HLEBcoin developers
 //
 // This file is part of Bytecoin.
 //
